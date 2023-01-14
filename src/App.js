e8493157fd83fc8b80c27431ac470541023ac497
Mandragid <mandragid@MacBook-Pro-Macbook.local>
2023-01-14 12:05:48 +0700

    hotfix

--- src/App.js
+++ src/App.js
@@ -4,29 +4,34 @@ import { handleAdd, handleSub } from "./redux/actions/counterAction";
 import { handleLogin } from "./redux/actions/authAction";
 
 function App() {
+  // receive state from redux, get all state needed from rootreducer
+
   const { counter, login } = useSelector((rootReducer) => rootReducer);
   const dispatch = useDispatch();
 
-  console.log(login);
+  // receive function from redux
 
-  const add = () => {
+  const handleAdd = () => {
     dispatch(handleAdd(counter));
   };
 
-  const sub = () => {
+  const handleSub = () => {
     dispatch(handleSub(counter));
   };
   const handleLoginButton = () => {
     dispatch(handleLogin(login));
   };
+
   return (
     <div className="Counter">
-      <button onClick={add}>+</button>
-      <button onClick={sub}>-</button>
+      <button onClick={handleAdd}>+</button>
+      <button onClick={handleSub}>-</button>
       <h1>{counter.total}</h1>
-      {login.isLogin ? <h1>anda sudah login</h1> : <h1>silahkan login</h1>}
+      {/* login condition message using ternary */}
+      {login.isLogin ? <h1>You Have Logged In</h1> : <h1>Please Login</h1>}
+      {/* login/logout button based on login state */}
       <button onClick={handleLoginButton}>
-        {login.isLogin ? "logout" : "login"}{" "}
+        {login.isLogin ? "LOGOUT" : "LOGIN"}{" "}
       </button>
     </div>
   );
--- src/redux/actions/authAction.js
+++ src/redux/actions/authAction.js
@@ -1,8 +1,7 @@
 export const handleLogin = (login) => (dispatch) => {
+  // negation boolean
   let loginState = login.isLogin;
-
   loginState = !loginState;
-
   dispatch({
     type: "AUTH",
     payload: loginState,
